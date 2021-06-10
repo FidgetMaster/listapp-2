8195d1b6484fc6a76066f66328f4d1951f192ed9
FidgetMaster <75333039+FidgetMaster@users.noreply.github.com>
2021-06-10 12:53:10 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Homescreen from './screens/homescreen';
+import Formscreen from './screens/formscreen';
+
+export default function App() {
+  return (
+    <AppContainer/>
+  )
+}
+const TabNavigator = createBottomTabNavigator({
+  Homescreen:{screen:Homescreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("./assets/homeicon.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Lists",
+      tabBarOptions: {activeTintColor: "#8c6cd8", inactiveTintColor: "#cece7b"}
+    }},
+  Formscreen:{screen:Formscreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("./assets/formicon.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Create List",
+      tabBarOptions: {activeTintColor: "#8c6cd8", inactiveTintColor: "#cece7b"}
+    }}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
+
---
+++ components/MyHeader.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import { Header,Icon,Badge } from 'react-native-elements';
+import { View, Text, StyeSheet ,Alert} from 'react-native';
+
+const MyHeader = props => {
+  return (
+    <Header
+      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
+      centerComponent={{ text: props.title, style: { color: '#cece7b', fontSize:20,fontWeight:"bold", fontFamily:'Arial',} }}
+      backgroundColor = "#8c6cd8"
+    />
+  );
+};
+
+export default MyHeader;
